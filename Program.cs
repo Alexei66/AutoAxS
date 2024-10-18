49d83d1266af31aef84fed2167d86f2f4074a786
Alexei66 <borisenkosenee@gmail.com>
2024-10-18 13:55:41 +0300

    TurboGrid (CFX, FLUENT) работает

--- Program.cs
+++ Program.cs
@@ -29,7 +29,10 @@ namespace ConsoleApp1
             driver = new WindowsDriver(appiumLocalService, desiredCapabilities);
 
             //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            Thread.Sleep(2000);
+            //Thread.Sleep(2000);
+
+            driver.Manage().Window.Maximize();
+
             driver.FindElement(By.XPath("//*[@Name= ' File    ']")).Click();
             driver.FindElement(By.XPath("//*[@Name= 'Open...\tCtrl+O']")).Click();
             driver.FindElement(By.XPath("//*[@AutomationId='1001']")).Click();
@@ -37,29 +40,104 @@ namespace ConsoleApp1
             driver.FindElement(By.XPath("//*[@AutomationId= '41477']")).SendKeys(Keys.Return);
             driver.FindElement(By.XPath("//*[@AutomationId= '1148']")).SendKeys($@"Mark15_inducer.axx");
             driver.FindElement(By.XPath("//*[@AutomationId= '1' and @Name= 'Open']")).Click();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.FindElement(By.XPath("//*[@Name= 'Viewer']")).Click();
+
+            // кнопка с 3D видом
 
             var element = driver.FindElement(By.XPath("//*[@AutomationId='59664']"));
             element.Click();
             element.SendKeys(Keys.Shift + Keys.F10); // замена ПКМ
 
+            //var DesktopSession = new AppiumServiceBuilder()
+            //    .UsingPort(4723)
+            //    .Build();
+            //DesktopSession.Start();
+            //AppiumOptions DesktopCapabilities = new AppiumOptions();
+            //DesktopCapabilities.App = "Root";
+            //DesktopCapabilities.AutomationName = "Windows";
+            WindowsDriver driver2 = ContextWindow();
+
+            var addWatcherElement = driver2.FindElement(By.XPath("//*[@Name= 'Export ...']"));
+            //WebDriverWait Desktopwait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            //Desktopwait.Until(pred => AddWatcherElement.Displayed);                           // задержка пока что-то не откроется
+            addWatcherElement.Click();
+
+            /////////TurboGrid (CFX, FLUENT)/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'TurboGrid (CFX, FLUENT)']")).Click();
+            driver.FindElement(By.XPath("//*[@Name= 'Path...']")).Click();
+
+            driver.FindElement(MobileBy.XPath("/Window/Window/Window/Pane[2]/Pane[3]/ProgressBar/Pane/ToolBar")).Click();
+            driver.FindElement(By.XPath("//*[@Name= 'Address']")).SendKeys(@"D:\EXPORT_IMPORT\New folder");
+            driver.FindElement(By.XPath("//*[@AutomationId= '41477']")).SendKeys(Keys.Return);
+
+            driver.FindElement(By.XPath("//*[@Name= 'Save']")).Click();
+            driver.FindElement(By.XPath("//*[@AutomationId= '1' and @Name= 'OK']")).Click();
+
+            /////////Gambit (FLUENT)/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'Gambit (FLUENT)']")).Click();
+
+            /////////AutoGrid (NUMECA)/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'AutoGrid (NUMECA)']")).Click();
+
+            /////////3D Components/////////
+
+            driver.FindElement(By.XPath("//*[@Name= '3D Components']")).Click();
+
+            /////////Geometry for External CFD/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'Geometry for External CFD']")).Click();
+
+            /////////STL/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'STL']")).Click();
+
+            /////////ProE IBL/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'ProE IBL']")).Click();
+
+            /////////AxCFD/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'AxCFD']")).Click();
+
+            /////////esTurbo (Star-CCM+)/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'esTurbo (Star-CCM+)']")).Click();
+
+            /////////LSD IMP/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'LSD IMP']")).Click();
+
+            /////////CAM EXPORT/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'CAM EXPORT']")).Click();
+
+            /////////Z Plane/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'Z Plane']")).Click();
+
+            /////////ANSYS WorkBench/////////
+
+            driver.FindElement(By.XPath("//*[@Name= 'ANSYS WorkBench']")).Click();
+
+            //driver.Quit();
+        }
+
+        private static WindowsDriver ContextWindow()
+        {
             var DesktopSession = new AppiumServiceBuilder()
                 .UsingPort(4723)
                 .Build();
             DesktopSession.Start();
+
             AppiumOptions DesktopCapabilities = new AppiumOptions();
             DesktopCapabilities.App = "Root";
             DesktopCapabilities.AutomationName = "Windows";
-            WindowsDriver driver2 = new WindowsDriver(DesktopSession, DesktopCapabilities);
 
-            var AddWatcherElement = driver2.FindElement(By.XPath("//*[@Name= 'Export ...']"));
-            //WebDriverWait Desktopwait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            //Desktopwait.Until(pred => AddWatcherElement.Displayed);                           // задержка пока что-то не откроется
-            AddWatcherElement.Click();
-            driver.FindElement(By.XPath("//*[@Name= '3D Components']")).Click();
-
-            //driver.Quit();
+            return new WindowsDriver(DesktopSession, DesktopCapabilities);
         }
     }
 }
